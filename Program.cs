d47109fd705bd3503d9685e8af0ef8c507b906e3
Redo From Start <55228370+Redo-From-Start@users.noreply.github.com>
2019-11-21 11:22:00 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace retro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            slow("Hello, World!", 30);
+            Console.ReadKey();
+        }
+        public static void slow(string say, int time = 10)
+        {
+            for (int i = 0; i < say.Length; i++)
+            {
+                Console.Write(say[i]);
+                Thread.Sleep(time);
+            }
+            return;
+        }
+    }
+}
